38830d692a33df7635c6712721b3cb89c04ca44f
Steven <steven.acosta867@gmail.com>
2025-07-17 19:44:46 -0400

    fixed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+  // This ensures that asset paths are generated correctly for production
+  base: '/', 
 })
